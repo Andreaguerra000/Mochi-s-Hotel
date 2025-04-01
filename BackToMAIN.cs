f6239bc7a20c6d3e679dde03b212e95649d8f08b
Andrea Guerra <andreaguerraa2000@gmail.com>
2025-04-01 14:24:44 -0500

    Update BackToMAIN.cs
    comments

--- BackToMAIN.cs
+++ BackToMAIN.cs
@@ -3,8 +3,7 @@ using UnityEngine.SceneManagement;
 public class BackToMAIN : MonoBehaviour
 {
       /*
-      
-      
+      this function checks to see if player wants to go back outside to the main area
       */
    public GameObject backToMain;
       public void OnTriggerEnter(Collider other){
